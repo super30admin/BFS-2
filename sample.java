ba93417f193da3ea7b2e0fc72b0707b00cc51c11
Abhinav <hiabhi95@gmail.com>
2019-08-27 01:08:01 -0700

    Completed Problem 55 : Please Review

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 //Time Complexity - O[N]
-//Space Complexity - O[1]
+//Space Complexity - O[N]
 
 
 /**
@@ -12,34 +12,21 @@
  * }
  */
 class Solution {
-    //Storing the parent values and the depths of the given x and y in variables
-    int xdepth;
-    int xparent;
-    int ydepth;
-    int yparent;
-
-    public boolean isCousins(TreeNode root, int x, int y) {
-        traverse(null, root, 0, x, y);
-
-        #Checking if the depths of x and y are equal and the parent of x and y are not the same
-        return xdepth != 0 && ydepth != 0 && xdepth == ydepth && xparent != yparent;
+    
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> l = new ArrayList();
+        help(root, l, 0);
+        return l;
     }
-
-    //Traversing through the entire tree and increasing the depth by 1 for each traversal
-    public void traverse(TreeNode parent, TreeNode node, int depth, int x, int y) {
-        if (node == null)
+    
+    public void help(TreeNode root, List<Integer> l, int level){
+        if(root == null)
             return;
-
-        if (node.val == x) {
-            xdepth = depth;
-            xparent = parent == null? 0: parent.val;
+        if(l.size() == 0 || l.size() <= level){
+            l.add(root.val);
         }
-        if (node.val == y) {
-            ydepth = depth;
-            yparent = parent == null? 0: parent.val;
-        }
-
-        traverse(node, node.left, depth + 1, x, y);
-        traverse(node, node.right, depth + 1, x, y);
+		//add the right-most side first.
+        help(root.right, l, level+1);
+        help(root.left, l, level+1);
     }
 }
