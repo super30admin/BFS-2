a2ef1e4ca8d71514f44cef4e7129e49ed27046f2
Mudita Bysani <muditabysani@Muditas-MacBook-Pro.local>
2019-08-27 08:24:06 -0700

    Completed optimized version of Problem1

--- Problem1.py
+++ Problem1.py
@@ -4,9 +4,10 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque
 
 class Solution:
-	def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+	def isCousins1(self, root: TreeNode, x: int, y: int) -> bool:
 		# Time Complexity : O(n) where n is the number of nodes in the tree
 		# Space Complexity : O(n) where n is the number of nodes in the tree
 		NodeParent = dict() # to store the parent of every node in the tree
@@ -19,3 +20,73 @@ class Solution:
 				FindParentDepth(node.right, node)
 		FindParentDepth(root, None) # Parent of root node is None
 		return NodeDepth[x] == NodeDepth[y] and NodeParent[x] != NodeParent[y]
+
+	def isCousins2(self, root, x, y):
+		queue = deque()
+		queue.append(root)
+		while len(queue) > 0:
+			q_size = len(queue)
+			x_found = False
+			y_found = False
+			for i in range(q_size):
+				node = queue.popleft()
+				if node.left != None and node.right != None:
+					if (node.left.val == x and node.right.val == y) or (node.right.val == x and node.left.val == y):
+						return False
+				if node.val == x:
+					x_found = True
+				elif node.val == y:
+					y_found = True
+				if node.left != None:
+					queue.append(node.left)
+				if node.right != None:
+					queue.append(node.right)
+			if x_found and y_found:
+				return True
+		return False
+
+	def isCousins3(self, root, x, y):
+		queue = deque()
+		queue.append(root)
+		while len(queue) > 0:
+			parentset = set()
+			q_size = len(queue)
+			for i in range(q_size):
+				node = queue.popleft()
+				if node.left != None:
+					if node.left.val == x:
+						parentset.add(node.val)
+					elif node.left.val == y:
+						parentset.add(node.val)
+				if node.right != None:
+					if node.right.val == x:
+						parentset.add(node.val)
+					elif node.right.val == y:
+						parentset.add(node.val)
+				if node.left != None:
+					queue.append(node.left)
+				if node.right != None:
+					queue.append(node.right)
+			if len(parentset) == 2:
+				return True
+		return False
+
+	def isCousins4(self, root, x, y):
+		queue = deque()
+		queue.append(root)
+		while len(queue) > 0:
+			parentset = set()
+			q_size = len(queue)
+			for i in range(q_size):
+				node = queue.popleft()
+				if node.left != None and (node.left.val == x or node.left.val == y):
+						parentset.add(node.val)
+				if node.right != None and (node.right.val == x or node.right.val == y):
+						parentset.add(node.val)
+				if node.left != None:
+					queue.append(node.left)
+				if node.right != None:
+					queue.append(node.right)
+			if len(parentset) == 2:
+				return True
+		return False
