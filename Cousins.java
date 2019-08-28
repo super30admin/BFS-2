68cc3f1e38df63fcf0f2d27ef63a9adee1967309
Manogna Mujje <manogna.mujje@gmail.com>
2019-08-27 20:46:24 -0700

    Added solution without using HashSet

--- Cousins.java
+++ Cousins.java
@@ -51,5 +51,34 @@ public class Cousins {
 			}
 	        return false;
     }
+	
+	//Optimized version without using HashSet
+	public static boolean isCousinsOptimized(TreeNode root, int x, int y) {
+		 if(root == null) return true;
+			Queue<TreeNode> q = new LinkedList<>();
+			boolean xFound = false, yFound = false;
+			q.add(root);
+			while(!q.isEmpty()) {
+				int count = q.size();
+				//For each level
+				for(int i=0; i<count; i++) {
+					TreeNode curr = q.remove();
+					if(curr.left != null) q.add(curr.left);
+					if(curr.right != null) q.add(curr.right);
+					//If x and y belong to same parent, they're not cousins
+					if(curr.left != null && curr.right != null) {
+						if((curr.left.val == x && curr.right.val == y) ||  (curr.left.val == y && curr.right.val == x)) {
+							return false;
+						}
+					}
+					if(curr.val == x) xFound = true;
+					if(curr.val == y) yFound = true;
+					if(xFound && yFound) return true;
+				}
+				xFound = false;
+				yFound = false;
+			}
+	        return xFound && yFound;
+   }
 
 }
