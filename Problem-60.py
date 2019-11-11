952b41fa1d1d8a55fa9e2f44e954785cf9e923ff
sindhu819 <sindu819@gmail.com>
2019-11-10 16:55:18 -0800

    Update Problem-60.py

--- Problem-60.py
+++ Problem-60.py
@@ -48,4 +48,42 @@ class Solution(object):
         return minutes if fresh==0 else -1 # when fresh is 0 we return minutes 
             
                               
+            
+# BFS Solution - without valid matrix            
+class Solution(object):
+    def orangesRotting(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        queue = [] 
+        m = len(grid) 
+        n = len(grid[0])
+        # edge case
+        if m == 0: return 0
+        fresh = 0 # count of fresh oranges.
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2: queue.append([i,j]) 
+                if grid[i][j] == 1: fresh += 1 
+        dirs = [[1,0],[0,1],[-1,0],[0,-1]]  
+        count_minutes = 0
+        while queue: 
+            size = len(queue) 
+            for k in range(size): 
+                rotten = queue.pop(0) 
+                for dir in dirs:
+                    i = dir[0] + rotten[0]
+                    j = dir[1] + rotten[1]
+                    if i >= 0 and i < m and j >= 0 and j < n and grid[i][j] == 1:
+                        queue.append([i,j])
+                        grid[i][j] = 2
+                        fresh -= 1
+            count_minutes += 1 
+        if fresh > 0: 
+            return -1
+        elif count_minutes > 0:
+            return count_minutes - 1
+        else:
+            return 0
         
