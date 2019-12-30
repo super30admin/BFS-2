36035032215d8f93fa206e4309f3b01b8c8354a6
Sweety Sojrani <sweety.sojrani@sjsu.edu>
2019-12-29 16:36:01 -0800

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -26,3 +26,25 @@ class Solution {
         return result;
     }
 }
+
+// DFS
+// Time Complexity - O(n)
+// Space Complexity - O(1) since we are not using any extra space apart from the recursion stack.
+// This solution worked on LeetCode
+// In this DFS solution, the root is processed and then it's right child is processed first since we have to get the right side view of the tree. Once the right childs are completed, we move to the left side. For adding node it is checked if the node depth is equal to the result size. If it is equal it means the node will be visible from right side view and the existing elements in the result list will not obstruct the view. If the depth is less than the result list size it means the elements in the result list will obstruct the view of this node. The recursive solution will process all the right children first and then all the left children of the tree. Similarly if we need the left view of the tree we will process all the left children first and then the right child nodes.
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null)    return result;
+        helper(root,0,result);
+        return result;
+    }
+    private void helper(TreeNode root, int depth,List<Integer> result){
+        if(root == null)    return;
+        if(result.size() == depth)  
+            result.add(root.val);
+        helper(root.right, depth+1, result);
+        helper(root.left, depth +1, result);
+    }
+}
