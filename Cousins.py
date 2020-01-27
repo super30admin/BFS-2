57c112375099ee1aa07df993a8992a0fcf6e74b6
Neha Sharma <53287893+neha-0507@users.noreply.github.com>
2020-01-26 18:36:16 -0700

    Update Cousins.py
    For Bfs my output is not correct and don't know about the mistake pleas have a look.

--- Cousins.py
+++ Cousins.py
@@ -27,3 +27,40 @@ class Solution:
         self.dfs(root.left,x,y,depth+1,root)
         self.dfs(root.right,x,y,depth+1,root)
         
+////BFS
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        if(root==None):
+            return False
+        q=[]
+        q.append(root)
+
+        while q:
+            x=False
+            y=False
+            size=len(q)
+            for i in range(size):
+                node=q.pop()
+                if(node.val==x):
+                    x=True
+                if(node.val==y):
+                    y=True
+                if((node.left!=None) and (node.right!=None)):
+                    if((node.left.val==x) and (node.right.val==y)):
+                        return False
+                    if((node.right.val==x) and (node.left.val==y)):
+                        return False
+                if((node.left!=None)):
+                    q.append(node.left)   
+                if((node.right!=None)):
+                    q.append(node.right)
+            if((x) and (y)):
+                return True
+        return False
