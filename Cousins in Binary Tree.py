8a06cc98e47cab49ced2abfe40b00de948c77e5c
Sowndhariya Nandarajkumar <41890348+SoundNandu@users.noreply.github.com>
2020-03-05 14:24:21 -0800

    Update Cousins in Binary Tree.py

--- Cousins in Binary Tree.py
+++ Cousins in Binary Tree.py
@@ -1,1 +1,47 @@
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
+class Solution(object):
+    def isCousins(self, root, x, y):
+        """
+        :type root: TreeNode
+        :type x: int
+        :type y: int
+        :rtype: bool
+        """
+        result = []
+        if root == None:
+            return False
+        queue = deque()
+        queue.append(root)
+        while queue:
+            size = len(queue)
+            x_found,y_found = False ,False
+            for _ in range(size):
+                node = queue.popleft() 
+                if (node.val) == x :
+                    x_found = True
+                if (node.val) == y:
+                    y_found = True
+                if not node.left or not node.right:
+               # if (node.left != null or node.right != null ):
+                    print(node.left.val)
+                    if node.left.val == x or node.right.val == y:
+                        return False
+                    if node.right.val == y or node.left.val == x:
+                        return False
+                if (node.left != None):
+                    queue.append(node.left)
+                if(node.right != None):
+                    queue.append(node.right)
+            if (x_found and y_found ):
+                return True
+            if (x_found or y_found ):
+                return False
+        return Fasle
+                    
