68c09a10b65cda7ddee8f8ca2a10129094371a33
Sonali B Sreedhar <50754966+sbsreedh@users.noreply.github.com>
2020-05-21 12:59:04 -0700

    Create Employee.py

---
+++ Employee.py
@@ -0,0 +1,28 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+#Time Complexity:O(v+e)
+#Space Comlexity:O(n)
+
+#DFS Solution:
+
+#Algorithm:
+# 1. Create a hashmap to store the employees and their objects.
+# 2. write a helper function to add the importance of the employee and their subordinates by calling them recursively. 
+# 3. return the result.
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        if not employees: return 0
+        hashMap={e.id:e for e in employees}
+        def helper(id):
+            result=hashMap[id].importance
+            for sub in hashMap[id].subordinates:
+                result+=helper(sub)
+            return result
+        return helper(id)
+        
