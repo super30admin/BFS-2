be50e36abcbf2481130df9e68ec630ac8ea0ba11
chirag <cr.tagadiya@gmail.com>
2020-06-24 20:57:43 -0400

    Cousing.java using BFS

--- Cousins.java
+++ Cousins.java
@@ -44,4 +44,59 @@ public class Cousins {
         
         dfs(root.right, depth + 1, root, x,y);
     }
+
+
+    /*
+        BFS approach
+    */
+
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if (root == null) return false;
+        
+        Queue<TreeNode> queue = new LinkedList();
+        
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            Boolean x_found = false;
+            Boolean y_found = false;
+            for(int i =0;i<size;i++){
+                TreeNode node = queue.poll();
+                if(node.val == x) x_found = true;
+                if(node.val == y) y_found = true;     
+                
+                if(node.left != null && node.right!=null){
+                    if(node.left.val == x && node.right.val == y) return false;
+                    if(node.right.val == x && node.left.val == y) return false;
+                }
+                
+                if(node.left != null){
+                    queue.add(node.left);
+                }
+                if(node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            
+            if(x_found && y_found ) return true;
+        }
+        
+        return false;
+    }
 }
