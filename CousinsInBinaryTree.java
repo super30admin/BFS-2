65ebd8f384e422709f7a85004442a7a824b4ea45
Arpit Sharma <arpit317@gmail.com>
2020-10-03 12:57:24 -0700

    Update CousinsInBinaryTree.java

--- CousinsInBinaryTree.java
+++ CousinsInBinaryTree.java
@@ -1,3 +1,6 @@
+//BFS
+//TC: O(n)
+//SC: O(width) = O(n/2)
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
         if(root == null) return false;
@@ -20,5 +23,38 @@ class Solution {
             }
         }
         return false;
+    }
+}
+
+//DFS
+//TC: O(n)
+//SC: O(h)
+class Solution {
+    TreeNode xParent; TreeNode yParent;
+    int xDepth; int yDepth;
+    
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if(root == null) return false;
+        dfs(root, 0, null, x, y);
+        return xParent != yParent && xDepth == yDepth;
+    }
+    
+    private void dfs(TreeNode root, int level, TreeNode parent, int x, int y){
+        //base
+        if(root == null) return;
+        
+        //logic
+        if(root.val == x){
+            xParent = parent;
+            xDepth = level;
+        }
+        
+        else if(root.val == y){
+            yParent = parent;
+            yDepth = level;
+        }
+        
+        dfs(root.left, level+1, root, x, y);
+        dfs(root.right, level+1, root, x, y);
     }
 }
