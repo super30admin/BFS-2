ef73f04d452b569e2761c262b76c11d558ad1cdb
Suyash906 <suyash90682711@gmail.com>
2020-10-03 13:50:04 -0700

    leetcode 993 and 994

--- 994_Rotting_Oranges.py
+++ 994_Rotting_Oranges.py
@@ -1,49 +1,34 @@
 from collections import deque
 class Solution:
-    def getOrangeDivision(self, grid):
-        fresh_orange_count = 0
-        rotten_orange_list = []
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        if not grid:return 0
+        
+        q = deque([])
+        
         for i, row in enumerate(grid):
             for j, col in enumerate(row):
-                if 1 == col:
-                    fresh_orange_count+=1
-                if 2 == col:
-                    rotten_orange_list.append((i,j))
-        return (fresh_orange_count, rotten_orange_list)
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return -1
+                if col == 2:
+                    q.append((i,j))
         
-        minutes = 1
-        total_fresh_oranges, rotten_orange_list = self.getOrangeDivision(grid)
-        if 0 == total_fresh_oranges:
-            return 0
-        visited = [[False for col in row] for row in grid]
-        neighbours = [[0,1], [1,0], [-1,0], [0,-1]]
-        row_max = len(grid)
-        col_max = len(grid[0])
-        
-        
-        q = deque()
-        temp_q = deque()
-        for orange in rotten_orange_list:
-            q.append(orange)
-        
+        m, n = len(grid), len(grid[0])
+        time = 0 
+        directions = [(-1,0), (0,1), (1,0), (0,-1)]
         while q:
-            x, y = q.popleft()
-            visited[x][y] = True
-            for neighbour in neighbours:
-                n_x = neighbour[0] + x
-                n_y = neighbour[1] + y
-                if n_x >= 0 and n_x < row_max and n_y >= 0 and n_y < col_max and 1 == grid[n_x][n_y] and not visited[n_x][n_y]:
-                    temp_q.append((n_x, n_y))
-                    grid[n_x][n_y] = 2
-                    visited[n_x][n_y] = True
-                    total_fresh_oranges-=1
-            if 0 == total_fresh_oranges:
-                return minutes
-            if not q:
-                minutes+=1
-                while temp_q:
-                    q.append(temp_q.popleft())
-        return minutes if 0 == total_fresh_oranges else -1
+            size = len(q)
+            flag = False
+            while size>0:
+                x, y = q.popleft()
+                for cx, cy in directions:
+                    nx = x + cx
+                    ny = y + cy
+                    if nx >=0 and nx < m and ny >=0 and ny < n and grid[nx][ny] == 1:
+                        flag = True
+                        grid[nx][ny] = 2
+                        q.append((nx, ny))
+                size-=1
+            if flag:time+=1
+        for i, row in enumerate(grid):
+            for j, col in enumerate(row):
+                if col == 1:
+                    return -1
+        return time
