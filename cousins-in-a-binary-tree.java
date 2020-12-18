edd1d7c8d47140a9938806a4728c037510834096
Rishi Raj <rishiraj824@gmail.com>
2020-12-17 20:31:29 -0500

    Update cousins-in-a-binary-tree.java

--- cousins-in-a-binary-tree.java
+++ cousins-in-a-binary-tree.java
@@ -36,3 +36,65 @@ class Solution {
         dfs(root.right, x, y, level + 1, root);
     }
 }
+
+// BFS
+// Time - O(N)
+// Space - O(N)
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if(root == null) {
+            return false;
+        }
+        
+        return bfs(root, x, y);
+    }
+    
+    private boolean bfs(TreeNode root, int x, int y) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        int levelX = 0;
+        int levelY = 0;
+        int level = 0;
+        TreeNode parent = null;  
+        
+        TreeNode parentX = parent;
+        TreeNode parentY = parent;
+        
+        boolean isSibling  = false;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i=0;i<size;i++) {
+                TreeNode ele = q.poll();
+                
+                if(x == ele.val) {
+                    levelX = level;
+                }
+                if(y == ele.val) {
+                    levelY = level;
+                }
+                if(ele.left != null && ele.left.val == x && ele.right!= null && ele.right.val == y) {
+                    return false;
+                }
+                if(ele.left != null && ele.left.val == y && ele.right!= null && ele.right.val == x) {
+                    return false;
+                }
+                if(ele.left != null) {
+                    q.add(ele.left);
+                }
+                if(ele.right != null) {
+                    q.add(ele.right);
+                }     
+                
+            }
+            
+            level++;     
+        }
+        if( levelX == levelY) {
+                return true;
+            }
+        else {
+            return false;
+        }
+    }
+}
