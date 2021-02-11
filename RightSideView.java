6b8d6726a46a6c5613626e4c9db24257503fb405
abhisheksharma0904 <76174342+abhisheksharma0904@users.noreply.github.com>
2021-02-12 00:13:57 +0530

    Create RightSideView.java

---
+++ RightSideView.java
@@ -0,0 +1,23 @@
+//BFS
+//time complexity - o(n)
+//space complexity - o(n)
+public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i =0; i<size; i++){
+                TreeNode curr = q.poll();
+                if(i == 0)
+                    result.add(curr.val);
+                if(curr.right != null) q.add(curr.right);
+                if(curr.left != null) q.add(curr.left);
+            }
+        }
+        return result;
+    } 
