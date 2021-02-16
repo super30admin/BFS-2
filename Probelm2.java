dfcd824b2fa7e0983cbc570efd6e6c1a94a39e36
Kapish Garg <kapish5467@gmail.com>
2021-02-16 11:59:08 -0800

    Completed

--- Probelm2.java
+++ Probelm2.java
@@ -1,39 +1,51 @@
 //Time complexity:O(n)
 //Space complexity:O(Max breadth)
-//Code ran on leetcode:No
-//Solution with approach:Trie implementing BFS but i guess it requires more space
+//Code ran on leetcode:Yes
+//Solution with approach:Making a pair at every node of ts parent and current node
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
-        Queue<TreeNode> q = new LinkedList<>();
+        Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
         if(root==null)
             return false;
-        q.add(root);
-        int index=0;
-        boolean foundx=false, foundy=false;
+        q.add(new Pair<TreeNode,Integer>(root,-1));
         int parentx=-1;
-        int parenty=-2;
+        int parenty=-1;
         while(!q.isEmpty())
         {
             int size=q.size();
             for(int i=0;i<size;i++)
             {
-                TreeNode n = q.poll();
-                if(n.val==x){foundx=true;parentx=(index)/2; }
-                if(n.val==y){foundy =true; parenty=(index)/2;}
-                if(n.left!=null) q.add(n.left);
-                else
-                    index++;
-                index++;
-                if(n.right!=null) q.add(n.right);
-                else
-                    index++;
-                index++;
+                Pair<TreeNode, Integer> pair= q.poll();
+                TreeNode n = pair.getKey();
+                int parent=pair.getValue();
+                if(n.val==x){parentx=parent;}
+                if(n.val==y){parenty=parent;}
+                if(n.left!=null) {q.add(new Pair<TreeNode,Integer>(n.left,n.val));}
+                if(n.right!=null) {q.add(new Pair<TreeNode,Integer>(n.right,n.val));}
+              
                 
             }
-            if((foundx!=foundy) || (parentx == parenty))
+            if((parentx!=-1 && parenty!=-1)&&(parentx != parenty) )
+                return true;
+            else if(parentx==-1 && parenty!=-1 || parentx!=-1 && parenty==-1)
                 return false;
         }
-        return true;
+        return false;
         
     }
 
