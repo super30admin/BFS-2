2c3930ae3fb066eac5829cbd8690625a245464e7
Manikandan Selvanathan <mskagesai.com>
2021-05-19 03:29:19 -0400

    Problem 1 added DFS solution

--- RightSideOfTheTree.java
+++ RightSideOfTheTree.java
@@ -2,8 +2,9 @@ import java.util.*;
 
 public class RightSideOfTheTree {
 
-    public List<Integer> rightSideView(TreeNode root) {
-        
+    public List<Integer> rightSideViewBFS(TreeNode root) {
+        //TC: O(N) - As we are traversing through all the elements
+        //SC: O(N) - Worst case it will have all the elements in the queue.
         List<Integer> result = new ArrayList<Integer>();
         if(root == null) return result;
         
@@ -24,9 +25,29 @@ public class RightSideOfTheTree {
         return result;
     }
 
+    public List<Integer> rightSideViewDFS(TreeNode root) {
+        //TC: O(N) - As we are traversing through all the elements
+        //SC: O(H) - Worst case it will have height equalent elements in side the queue.
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null) return result;
+        dfs(root, 0, result);
+        return result; //As result is valud by reference in java, we don't need to keep it in the global scope.
+    }
+
+    private void dfs(TreeNode root, int height, List<Integer> result) {
+        if(root == null) return;
+        if(result.size() == height) {
+            result.add(root.val);
+        }
+        dfs(root.right, height+1, result);
+        dfs(root.left, height+1, result);
+    }
+
     public static void main(String[] args) {
         RightSideOfTheTree rightSideOfTheTree = new RightSideOfTheTree();
+        TreeNode sample = new TreeNode().getSampleFive();
+        List<Integer>  result = rightSideOfTheTree.rightSideViewBFS(sample);
+        System.out.println(result);
         
-        rightSideOfTheTree.rightSideView(root);
     }
 }
