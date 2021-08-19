a730ac7bc0f0d7003420f9084e61171107b006cc
Durgesh Patil <patildurgesh1998@gmail.com>
2021-08-18 22:48:42 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -76,3 +76,43 @@ public:
         dfs(root->left,lvl+1);
     }
 };
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+  Time Complexity = O(N)
+ Space Complexity = O(h)
+ where N is the number of nodes in the tree and h is the height of the tree.
+ */
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> rightSideView(TreeNode* root) {
+        if(root==NULL)
+            return ans;
+        dfs(root,0);
+        return ans;
+    }
+    void dfs(TreeNode *root, int lvl)
+    {
+        //base
+        if(root==NULL)
+            return;
+        //logic
+        if(lvl==ans.size())
+            ans.push_back(root->val);
+        else
+            ans[lvl] = root->val;
+        dfs(root->left,lvl+1);
+        dfs(root->right,lvl+1);
+    }
+};
