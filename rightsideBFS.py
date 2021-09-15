c19fb3445e8382560a4a720cdd867abfc0458075
Saman Akhtar <33394840+saman-akhtar@users.noreply.github.com>
2021-09-15 13:33:44 -0700

    Create rightsideBFS.py
    https://leetcode.com/problems/binary-tree-right-side-view/submissions/

---
+++ rightsideBFS.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# TC O(n)
+# SC O(n)
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        result = [] # to store integer
+        queue = deque([]) # to store TreeNode
+        queue.append(root)
+        while len(queue) > 0:
+            size = len(queue)
+            for i in range(size):
+                cur = queue.popleft()
+                if i == size - 1:
+                    result.append(cur.val)
+                if cur.left is not None:
+                    queue.append(cur.left)
+                if cur.right is not None:
+                    queue.append(cur.right)
+        return result
+        
+                    
+            
