2d5d67860b289dba2dfdb48635887860704fbc68
SaiGowtham-11 <61152047+SaiGowtham-11@users.noreply.github.com>
2021-12-22 13:56:43 -0600

    Cousins in Binary Tree

--- main.cpp
+++ main.cpp
@@ -18,35 +18,38 @@ using namespace std;
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int> result;//1
-        queue<TreeNode*> q;//
+    int X_value;
+    int Y_value;
+    int X_level;
+    int Y_level;
+    int X_parent;
+    int Y_parent;
+    void dfs(TreeNode * root,int parent , int level){
         if(root == NULL){
-            return result;
+            return;
         }
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            for(int i = 0; i< size; i++){
-                TreeNode* temp = q.front();
-                if(i == size -1 ){
-                    result.push_back(temp->val);
-                }
-                q.pop();
-                if(temp->left != NULL){
-                    q.push(temp->left);
-                }
-                if(temp->right != NULL){
-                    q.push(temp->right);
-                }
-            }
+        if(root->val == X_value){
+            X_level = level;
+            X_parent = parent;
         }
-        return result;
+        if(root->val == Y_value){
+            Y_level = level;
+            Y_parent = parent;
+        }
+        dfs(root->left,root->val,level+1);
+        dfs(root->right,root->val,level+1);
+    }
+
+    bool isCousins(TreeNode* root, int x, int y) {
+        X_value = x;
+        Y_value = y;
+        dfs(root,0,0);
+        if(X_level == Y_level && X_parent != Y_parent){
+            return true;
+        }
+        return false;
     }
 };
-
-
-
 
 
 
