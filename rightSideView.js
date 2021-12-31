d195b6e158e5191bec338d3d1c982c869ce8eb26
Salman Karim <salmankarimzindani@hotmail.com>
2021-12-31 13:31:17 -0600

    [rightSideView] implements dfs approach

--- rightSideView.js
+++ rightSideView.js
@@ -1,9 +1,33 @@
 // Time Complexity : O(n)
-// Space Complexity : O(n/2)
+// Space Complexity : O(h)
 // Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this : no
 
+// DFS approach
+var rightSideView = function (root) {
+  const resultsArr = [];
+  if (!root) return resultsArr;
+
+  const dfs = (root, level) => {
+    // base condition
+    if (root === null) return;
+    if (resultsArr[level] === undefined) {
+      resultsArr.push(root.val);
+    }
+    // logic
+    dfs(root.right, level + 1);
+    dfs(root.left, level + 1);
+  };
+
+  dfs(root, 0);
+  return resultsArr;
+};
+
 // BFS approach
+// TC for this approach worse than dfs because it would be O(n+n).
+// Given the first n could be removed as a constant due to the asymptotic nature
+// of calculating complexities, it would become O(n), but truly it is slower if the
+// constant is not removed.
 var rightSideView = function (root) {
   const resultsArr = [];
   if (!root) return resultsArr;
@@ -13,7 +37,8 @@ var rightSideView = function (root) {
   while (queue.length) {
     let length = queue.length;
     while (length--) {
-      let root = queue.shift();
+      // O(n) tc - looping through all elements
+      let root = queue.shift(); // additional O(n) time since we have to move all elements to the left in the array
       // if length is 0, we add the right-viewed element to our results arr.
       if (!length) {
         resultsArr.push(root.val);
