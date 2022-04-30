bcf8ed0b14b7033b64567060f4ee1c901396a9c7
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-04-30 16:41:42 -0700

    Add files via upload

---
+++ RightSideView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class RightSideView
+    {
+        /**
+         * T.C: O(N) traversing through all the nodes
+         * S.C: O(h) stach will have max of hight of the tree element
+         */
+        List<int> result;
+        public IList<int> GetRightSideView(TreeNode root)
+        {
+            result = new List<int>();
+            if (root == null)
+                return result;
+
+            dfs(root, 0);
+
+            return result;
+        }
+
+        private void dfs(TreeNode root, int level)
+        {
+            if (root == null)
+                return;
+
+            if (result.Count == level)
+                result.Add(root.val);
+            else
+            {
+                result[level] = root.val;
+            }
+
+            dfs(root.left, level + 1);
+            dfs(root.right, level + 1);
+        }
+    }
+}
