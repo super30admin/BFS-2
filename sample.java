05d58d069cfef857c6e6fcd849180126eb239e8d
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-22 12:19:22 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -171,3 +171,56 @@ class Solution {
         dfs(root.right, root, level+1, x, y);
     }
 }
+
+//****COUSINS IN BST-DFS APROACH****
+//Time complexity:o(n);
+//Space complexity: o(n); 
+//Leetcode runnable:Y;
+//Any doubts:N;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            boolean x_flag=false;
+            boolean y_flag=false;
+            for(int i=0;i<size;i++)
+            {
+                TreeNode curr=q.poll();
+                if(curr.val==x) x_flag=true;
+                if(curr.val==y) y_flag=true;
+                if(curr.left!=null && curr.right!=null)
+                {
+                    if(curr.left.val==x && curr.right.val==y) return false;
+                    if(curr.left.val==y && curr.right.val==x) return false;
+                }
+                
+                if(curr.left!=null)q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
+            }
+            if(x_flag==true && y_flag==true) return true;
+            if(x_flag==true || y_flag==true) return false;
+        }
+        return false;
+        
+    }
+}
