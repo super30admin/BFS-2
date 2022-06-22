b9f9b4c24703fe0910bc1fc54f0abda4b1a13848
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-22 11:02:36 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -114,3 +114,60 @@ class Solution {
         dfs(root.right, level+1);
     }
 }
+
+//****COUSINS IN BST-DFS APROACH****
+//Time complexity:o(n);
+//Space complexity: o(n); space for the recursive stack;
+//Leetcode runnable:Y;
+//Any doubts:N;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode x_parent;
+    TreeNode y_parent;
+    int x_ht;
+    int y_ht;
+    public boolean isCousins(TreeNode root, int x, int y) {
+        dfs(root, null, 0,x ,y);
+        return x_parent!=y_parent && x_ht==y_ht;
+    }
+    
+    private void dfs(TreeNode root, TreeNode parent, int level, int x, int y)
+    {
+        //Base case
+        if(root==null)
+        {
+            return;
+        }
+        //Logic
+        if(root.val==x)
+        {
+            x_parent=parent;
+            x_ht=level;
+            
+        }
+        if(root.val==y)
+        {
+            y_parent=parent;
+            y_ht=level;
+        }
+        
+        dfs(root.left, root, level+1, x, y);
+        
+        dfs(root.right, root, level+1, x, y);
+    }
+}
