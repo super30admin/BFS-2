8d5f28d4c388b8a4414aba5d1e59704315f49f69
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-22 10:11:23 -0400

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,4 +1,4 @@
-//****RIGHT SIDE VIEW OF A BINARY SEARCH TREE****
+//****RIGHT SIDE VIEW OF A BINARY SEARCH TREE- BFS****
 
 //Time complexity-o(n);
 //Space complexity-O(d); d is diameter of tree;
@@ -55,5 +55,62 @@ class Solution {
         //returning the result
         return ans;
         
+    }
+}
+
+//****RIGHT SIDE VIEW OF A BINARY SEARCH TREE- DFS****
+
+//Time complexity-o(N);
+//Space complexity-O(H);
+//Leetcode runnable: Y;
+//Any doubts: N;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> result;
+    public List<Integer> rightSideView(TreeNode root) {
+        result=new ArrayList<>();
+        if(root==null) return result;
+        dfs(root, 0);
+            return result;
+        
+    }
+    
+    private void dfs(TreeNode root, int level)
+    {
+        int x=0;
+        //base case
+        if(root==null)
+        {
+            return;
+        }
+        
+        //logic
+        if(level==result.size())
+        {
+            result.add(root.val);
+        }
+        else
+        {
+            result.set(level, root.val);
+        }
+        
+        //go for left and then right
+        dfs(root.left,level+1);
+        dfs(root.right, level+1);
     }
 }
