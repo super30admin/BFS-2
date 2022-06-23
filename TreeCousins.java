5557787b057798eb0a5e65af2122a61a2f0b6832
Nethra1 <vijayadasnethra97@gmail.com>
2022-06-23 18:05:53 -0400

    BFS 2 - please review

---
+++ TreeCousins.java
@@ -0,0 +1,40 @@
+public class TreeCousins {
+    // TC is O(n)
+    // SC is O(n)
+    public boolean isCousins(TreeNode root, int x, int y) {
+        Queue<TreeNode> q = new LinkedList<>();
+        boolean x_found = false;
+        boolean y_found = false;
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            x_found = false;
+            y_found = false;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                if (curr.val == x)
+                    x_found = true;
+                if (curr.val == y)
+                    y_found = true;
+
+                if (curr.left != null && curr.right != null) {
+                    if (curr.left.val == x && curr.right.val == y)
+                        return false;
+                    if (curr.left.val == y && curr.right.val == x)
+                        return false;
+                }
+                if (curr.left != null)
+                    q.add(curr.left);
+                if (curr.right != null)
+                    q.add(curr.right);
+            }
+            if (x_found && y_found)
+                return true;
+            if (x_found || y_found)
+                return false;
+        }
+
+        return false;
+    }
+}
