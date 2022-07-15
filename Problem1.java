0eb38c3eb6b4fbd403d376cd6dc04a83c690448e
Prajna Puranik <prajnagp@gmail.com>
2022-07-15 09:14:25 -0700

    Completed BFS-2

---
+++ Problem1.java
@@ -0,0 +1,42 @@
+// Time Complexity : O(N)
+// Space Complexity : O(H) where H is height of the tree
+// Did this code successfully run on Leetcode : Yes
+
+//Problem statement: https://leetcode.com/problems/binary-tree-right-side-view/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    List<Integer> rightside = new ArrayList();
+
+    public void helper(TreeNode node, int level) {
+        if (level == rightside.size())
+            rightside.add(node.val);
+
+        if (node.right != null)
+            helper(node.right, level + 1);
+        if (node.left != null)
+            helper(node.left, level + 1);
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return rightside;
+
+        helper(root, 0);
+        return rightside;
+    }
+}
