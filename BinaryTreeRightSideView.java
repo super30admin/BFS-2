ada0fcdb0260606e95619ac24c6d466e94bae3d1
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-20 12:34:39 -0700

    Create BinaryTreeRightSideView.java

---
+++ BinaryTreeRightSideView.java
@@ -0,0 +1,27 @@
+//TC = O(n) 
+//SC = O(n)
+
+//Using BFS right side
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        q.add(root); 
+        while(!q.isEmpty()){
+            int size = q.size();
+            
+            for(int i=0; i<size; i++){
+                TreeNode  curr = q.poll();
+                if(i==size-1){
+                    result.add(curr.val);
+                }
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+            }
+        }
+        return result;
+    }
+}
