c06e78243e598d6f17f07fab32a3427458b19ffb
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-20 12:49:42 -0700

    Update BinaryTreeRightSideView.java

--- BinaryTreeRightSideView.java
+++ BinaryTreeRightSideView.java
@@ -58,3 +58,40 @@ class Solution {
         
     }
 }
+
+
+//TC = O(n) 
+//SC = O(h)
+
+//Using DFS right side
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        
+        
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+
+        dfs(root, 0, result);
+        return result;
+    }
+    private void dfs(TreeNode root, int depth, List<Integer> result){
+        //base
+          if(root == null) return; 
+        
+        //logic
+        //depth of a tree and result list size, initially both 0 and 0, so add root
+        if(depth == result.size()){
+            result.add(root.val);
+        } else{
+            result.set(depth, root.val); //replace
+        }
+        
+        dfs(root.left, depth+1, result);
+        dfs(root.right, depth+1, result);
+        
+        
+    }
+}
+
+
+
