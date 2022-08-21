09f0bce0801724384b1dd78002268d59edfa5b95
Javeed Sanganakal <21953322+javeedsanganakal@users.noreply.github.com>
2022-08-20 19:16:39 -0700

    Update SymmetricTree.java

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -58,3 +58,33 @@ class Solution {
         return true;
     }
 }
+
+
+
+//BFS using Stack
+
+//TC = O(n)
+//SC = O(n)
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+       if(root == null) return true;
+       Stack<TreeNode> q = new Stack<>();
+       q.add(root.left);
+       q.add(root.right);
+        
+        while(!q.isEmpty()){
+            TreeNode left = q.pop();
+            TreeNode right = q.pop();
+            
+            if(left == null && right == null) continue;
+            if(left == null || right == null || left.val != right.val) return false;
+            
+            q.add(left.left); q.add(right.right);
+            q.add(left.right); q.add(right.left);
+            
+            
+        }
+        
+        return true;
+    }
+}
