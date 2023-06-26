09fcc4d081235fecbff948534e0e6694b1f2255b
Sana Pathan <sanapathan28@gmail.com>
2023-06-25 20:35:36 -0700

    https://leetcode.com/problems/binary-tree-right-side-view/ https://leetcode.com/problems/cousins-in-binary-tree/

---
+++ BTCousin.java
@@ -0,0 +1,26 @@
+public class BTCousin {
+    TreeNode xParent;
+    TreeNode yParent;
+    int xLevel;
+    int yLevel;
+    public boolean isCousins(TreeNode root, int x, int y) {
+        dfs(root,x,y,0, null);
+        return xParent!=yParent && xLevel==yLevel;
+    }
+    private void dfs(TreeNode root, int x, int y, int level, TreeNode parent){
+        if(root==null)
+            return;
+
+        if(root.val==x){
+            xParent=parent;
+            xLevel=level;
+        }
+        if(root.val==y){
+            yParent=parent;
+            yLevel=level;
+        }
+
+        dfs(root.left,x,y,level+1,root);
+        dfs(root.right,x,y,level+1,root);
+    }
+}
