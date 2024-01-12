6061606768f28c2b6ca8cc6c211b3c9a2cb27365
Sahil Raut <sahilraut@Sahils-MacBook-Air.local>
2024-01-11 17:50:34 -0800

    Code done

---
+++ BTRightSide.java
@@ -0,0 +1,26 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root==null) return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                TreeNode curr = q.poll();
+                if(i==size-1) result.add(curr.val);
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
+            }
+        }
+        return result;
+    }
+}
