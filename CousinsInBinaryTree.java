fb18a9a2af1e8dbf85e9907865989737c8efa91b
Anish <anishsingh0106@gmail.com>
2024-02-21 16:26:56 -0500

    BFS solutions for both problems

--- CousinsInBinaryTree.java
+++ CousinsInBinaryTree.java
@@ -1,7 +1,8 @@
-// Time complexity: O(n)
-// Space complexity: O(h)
 
-//Approach: Find both items and check if their parents are different and heights are equal
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
 
 class CousinsInBinaryTree {
     TreeNode xParent;
@@ -9,6 +10,66 @@ class CousinsInBinaryTree {
     int xHeight;
     int yHeight;
 
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+
+    // Approach: BFS and if they are from the same parent return false
+    // else if both are found at one level return true
+    public boolean isCousinsBFS(TreeNode root, int x, int y) {
+        Queue<TreeNode> queue = new LinkedList();
+        // atleast two elements in the tree
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            // process elements at current level
+            boolean foundX = false;
+            boolean foundY = false;
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+
+                if (current.val == x) {
+                    foundX = true;
+                }
+
+                if (current.val == y) {
+                    foundY = true;
+                }
+
+                if (current.left != null && current.right != null) {
+                    // both x and y from the same parent
+                    if ((current.left.val == x) && (current.right.val == y)
+                            || (current.right.val == x) && (current.left.val == y)) {
+                        return false;
+                    }
+                }
+
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+            // current level traversed
+            // both found and not from the same parent
+            if (foundX && foundY) {
+                return true;
+            }
+            // one found
+            if (foundX || foundY) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(h)
+
+    // Approach: Find both items and check if their parents are different and
+    // heights are equal
     public boolean isCousins(TreeNode root, int x, int y) {
         dfs(root, null, x, y, 0);
         if (xHeight == yHeight && xParent != yParent) {
--- RightSideView.java
+++ RightSideView.java
@@ -1,13 +1,17 @@
-// Time complexity: O(n)
-// Space complexity: O(h)
-
-//Approach: Only adding items if this is the first time at a certain level
-
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
 
 class RightSideView {
     List<Integer> res;
+
+    // Time complexity: O(n)
+    // Space complexity: O(h)
+
+    // Approach: Only adding items if this is the first time at a certain level
 
     public List<Integer> rightSideView(TreeNode root) {
         this.res = new ArrayList();
@@ -28,4 +32,38 @@ class RightSideView {
         dfs(node.right, height + 1);
         dfs(node.left, height + 1);
     }
+
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+
+    // Approach: Level order traversal and add the last element to the res;
+
+    public List<Integer> rightSideViewBFS(TreeNode root) {
+        List<Integer> res = new ArrayList();
+        Queue<TreeNode> queue = new LinkedList();
+        if (root == null) {
+            return res;
+        }
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                if (i == size - 1) {
+                    res.add(current.val);
+                }
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+        }
+        return res;
+    }
+
 }
